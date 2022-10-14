68340e30cb334d70724b9b34e952f1984c6e447a
Zain Ali Shah <zainalishah51472@gmail.com>
2022-10-14 21:07:19 +0500

    Create grid.py

---
+++ Week#6/Python Files/grid.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+
+myLabel1 = Label(root, text="Hello World!").grid(row=0, column=0)
+myLabel2 = Label(root, text="My Name is Zain Ali Shah!").grid(row=1, column=0)
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=0)
+
+root.mainloop()
